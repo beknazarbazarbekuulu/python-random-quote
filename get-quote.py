9a42614f33b851f1e4e75e090903a960cd7063c1
beknazarbazarbekuulu <beknazarbazarbekuulu@protonmail.com>
2020-12-09 00:24:08 +0600

    Removed extra line (called a newline) when printing

--- get-quote.py
+++ get-quote.py
@@ -6,11 +6,12 @@ def get_quote():
   f.close()
 
   last = len(quotes) - 1
-  rnd = random.randint(0, last)
-  rnd2 = random.randint(0, last)
+  rnd = quotes[random.randint(0, last)]
+  rnd2 = quotes[random.randint(0, last)]
+  rnd_items = [rnd, rnd2]
 
-  print(quotes[rnd])
-  print(quotes[rnd2])
+  for item in rnd_items:
+    print(item, end='')
 
 if __name__== "__main__":
   get_quote()
